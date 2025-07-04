d47d27ff912653b0c1099c1241d7a884c659710d
infanasotku <infanasotku@gmail.com>
2025-07-04 21:53:22 +0700

    Added proxy service abc/impl

---
+++ app/contracts/services/proxy.py
@@ -0,0 +1,6 @@
+from abc import abstractmethod, ABC
+from uuid import UUID
+
+
+class ProxyService(ABC):
+    pass
---
+++ app/services/proxy.py
@@ -0,0 +1,7 @@
+from uuid import UUID
+
+from app.contracts.services.proxy import ProxyService
+
+
+class ProxyServiceImpl(ProxyService):
+    pass
