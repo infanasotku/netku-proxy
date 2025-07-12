b7a3379667f4c54f090a542862487ff913daa8ce
infanasotku <infanasotku@gmail.com>
2025-07-12 19:04:12 +0700

    Refactored create_channel to support multiple root certificates for secure connections

--- app/infra/grpc/channel.py
+++ app/infra/grpc/channel.py
@@ -1,6 +1,7 @@
 from contextlib import asynccontextmanager
 from logging import Logger
-from typing import AsyncIterator, Protocol, AsyncContextManager
+from pathlib import Path
+from typing import AsyncIterator, Protocol, AsyncContextManager, Sequence
 
 from grpc.aio import Channel, secure_channel, insecure_channel
 import grpc
@@ -14,12 +15,17 @@ async def create_channel(
     host: str,
     port: int,
     logger: Logger | None = None,
+    root_certificates: str | Sequence[str] | None = None,
 ) -> AsyncIterator[Channel]:
     addr = f"{host}:{port}"
 
+    if root_certificates is None:
+        root_certificates = [certifi.where()]
+    elif isinstance(root_certificates, str):
+        root_certificates = [root_certificates]
+
     if with_cert:
-        with open(certifi.where(), "rb") as f:
-            cert = f.read()
+        cert = b"".join(Path(path).read_bytes() for path in root_certificates)
         creds = grpc.ssl_channel_credentials(cert)
         channel = secure_channel(addr, creds)
     else:
@@ -40,13 +46,19 @@ class CreateChannelContext(Protocol):
 def generate_create_channel_context(
     logger: Logger | None = None,
     with_cert=True,
+    *,
+    root_certificates: str | Sequence[str] | None = None,
 ) -> CreateChannelContext:
     @asynccontextmanager
     async def create_channel_context(addr: str) -> AsyncIterator[Channel]:
         host, port = addr.split(":")
 
         async with create_channel(
-            with_cert, host=host, port=int(port), logger=logger
+            with_cert,
+            host=host,
+            port=int(port),
+            logger=logger,
+            root_certificates=root_certificates,
         ) as channel:
             yield channel
 
