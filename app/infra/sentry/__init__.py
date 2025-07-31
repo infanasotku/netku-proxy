9e0052dd4c70bc9be9326cf273c7be0c4eefa7d5
infanasotku <infanasotku@gmail.com>
2025-07-31 22:48:08 +0700

    Refactor Sentry integration to include a customizable traces sampler

--- app/infra/sentry/__init__.py
+++ app/infra/sentry/__init__.py
@@ -1,5 +1,18 @@
+from typing import Any, Callable
+
 import sentry_sdk
 
 from app.infra.config.settings import settings
 
-sentry_sdk.init(dsn=settings.sentry.dsn, traces_sample_rate=1.0, debug=True)
+
+def init_sentry(*, traces_sampler: Callable[[Any], float] | None = None):
+    traces_sample_rate = None
+    if traces_sampler is None:
+        traces_sample_rate = 1.0
+
+    sentry_sdk.init(
+        dsn=settings.sentry.dsn,
+        traces_sampler=traces_sampler,
+        traces_sample_rate=traces_sample_rate,
+        send_default_pii=True,
+    )
