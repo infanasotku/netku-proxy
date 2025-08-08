e9fa9530f886e2d13b2d0bc885e82ef09bbdadb1
infanasotku <infanasotku@gmail.com>
2025-08-09 00:11:33 +0700

    Add Sentry transaction handling and update traces_sampler logic

--- app/entrypoints/api.py
+++ app/entrypoints/api.py
@@ -1,12 +1,26 @@
 from contextlib import asynccontextmanager
+from typing import cast
 
 from fastapi import FastAPI
+from sentry_sdk.types import Event
+from sentry_sdk.tracing import TransactionSource
 
 from app.infra.config import settings
 from app.infra.sentry import init_sentry
 from app.container import Container
 
 from app.controllers.admin import create_admin
+
+
+def before_send_transaction(event: Event, _):
+    if tr_info := event.get("transaction_info"):
+        source: TransactionSource = cast(TransactionSource, tr_info.get("source"))
+        if source == TransactionSource.URL:
+            return  # Cancel transactions for 404
+    else:
+        return
+
+    return event
 
 
 def traces_sampler(ctx: dict):
@@ -17,9 +31,6 @@ def traces_sampler(ctx: dict):
     if path.startswith("/admin") and not (
         path.startswith("/admin/engine/edit") and method == "POST"
     ):
-        return 0.0
-
-    if "favicon.ico" in path or ".well-known" in path:
         return 0.0
 
     return 1.0
@@ -51,7 +62,9 @@ def create_app() -> FastAPI:
         ]
     )
 
-    init_sentry(traces_sampler=traces_sampler)
+    init_sentry(
+        traces_sampler=traces_sampler, before_send_transaction=before_send_transaction
+    )
 
     app = FastAPI(redoc_url=None, docs_url=None, lifespan=create_lifespan(container))
     app.__dict__["container"] = container
