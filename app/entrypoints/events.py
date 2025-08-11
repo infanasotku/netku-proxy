5a6550ca08e5e3dbaf13d126ca76a64401ae5a72
infanasotku <infanasotku@gmail.com>
2025-08-11 15:03:26 +0700

    Refactor Rabbit and Redis broker functions; enhance lifespan management in event entrypoints

--- app/entrypoints/events.py
+++ app/entrypoints/events.py
@@ -2,33 +2,34 @@ from contextlib import asynccontextmanager
 
 from faststream.asgi import make_ping_asgi
 from faststream import FastStream
-from faststream.redis import RedisBroker
-from dependency_injector import providers
 
 from app.infra.config import settings
 from app.infra.sentry import init_sentry
 from app.infra.logging import logger
-from app.container import Container
+from app.container import Container, EventsResource
 
 from app.controllers.events import engine
 
 
-def create_lifespan(container: Container, engine_broker: RedisBroker):
-    @asynccontextmanager
-    async def lifespan(_):
-        redis = await engine_broker.connect()  # For getting redis instance
-        container.redis.override(providers.Singleton(lambda: redis))
-
-        future = container.init_resources()
+def create_lifespan(container: Container, app):
+    async def _maybe_future(future):
         if future is not None:
             await future
 
+    @asynccontextmanager
+    async def lifespan():
+        engine_broker = await container.redis_broker()
+        engine_broker.include_router(engine.router)
+
+        app.mount("/healthz", make_ping_asgi(engine_broker, timeout=5.0))
+        app.broker = engine_broker
+
+        redis = await container.redis()
+
+        await _maybe_future(container.init_resources(EventsResource))
         async with engine.start_keyevents_reclaimer(redis, logger):
             yield
-
-        future = container.shutdown_resources()
-        if future is not None:
-            await future
+        await _maybe_future(container.shutdown_resources(EventsResource))
 
     return lifespan
 
@@ -44,20 +45,16 @@ def create_app():
 
     init_sentry()
 
-    engine_broker = RedisBroker(settings.redis.dsn, logger=logger)
-    engine_broker.include_router(engine.router)
-
     app = FastStream(
-        engine_broker,
+        None,
         logger=logger,
-        lifespan=create_lifespan(container, engine_broker),
         title="Proxy events",
         version="",
         identifier="urn:events",
     ).as_asgi(
         asyncapi_path=None,
-        asgi_routes=[("/healthz", make_ping_asgi(engine_broker, timeout=5.0))],
     )
+    app.lifespan_context = create_lifespan(container, app)
     app.__dict__["container"] = container
 
     return app
