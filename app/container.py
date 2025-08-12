307831bbad9ccb75459b462d9ff6fcd487eecc65
infanasotku <infanasotku@gmail.com>
2025-08-12 11:01:32 +0700

    Enhance RedisBroker initialization with socket options and health check settings

--- app/container.py
+++ app/container.py
@@ -39,7 +39,15 @@ def get_rabbit_publisher(broker: RabbitBroker, *, queue: RabbitQueue):
 
 
 async def get_redis_broker(dsn: str, *, db: int = 0):
-    broker = RedisBroker(dsn, db=db)
+    broker = RedisBroker(
+        dsn,
+        db=db,
+        health_check_interval=10,
+        retry_on_timeout=True,
+        # Socket options
+        socket_connect_timeout=5,
+        socket_keepalive=True,
+    )
     await broker.connect()
     try:
         yield broker
