248106c0f4548a0e96c131155274968cf84f0cf0
infanasotku <infanasotku@gmail.com>
2025-08-15 14:09:12 +0700

    Refactor engine schemas to enhance EngineInfoDTO and update EngineCmd inheritance

--- app/schemas/engine.py
+++ app/schemas/engine.py
@@ -4,9 +4,13 @@ from uuid import UUID
 from app.schemas import BaseSchema
 
 
-class EngineCmd(BaseSchema):
+class EngineInfoDTO(BaseSchema):
     id: UUID
     created: datetime
     running: bool = False
     uuid: UUID | None = None
     addr: str
+
+
+class EngineCmd(EngineInfoDTO):
+    pass
